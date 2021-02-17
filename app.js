0f15000523bbd6f5e86cd717f3fd72f404181115
klishin.nd16 <klishin.nd16@gmail.com>
2021-02-17 17:37:53 +0300

    Change 04

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const authRouter = require('./routes/auth.routes')
 
 
 const app = express()
-const PORT = config.get('port') || 5000
+// const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({ extended: true }))
 app.use(express.static('public'))
