7f01effa25d101bf4a3195aa8514c092d4e6c56b
klishin.nd16 <klishin.nd16@gmail.com>
2021-02-17 17:55:13 +0300

    Change 09

---
+++ client/build/precache-manifest.0a4139a990d166abc7bd5523476da215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd386ede915e16e3f2f08434ed84ef4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a80f811e54ade9f18d6",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "d34355a7d302241d1f3a",
+    "url": "/static/css/main.fbd794c3.chunk.css"
+  },
+  {
+    "revision": "5a80f811e54ade9f18d6",
+    "url": "/static/js/2.a73fe489.chunk.js"
+  },
+  {
+    "revision": "9cbaa7e13ab69a72b94de82056a31ae9",
+    "url": "/static/js/2.a73fe489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34355a7d302241d1f3a",
+    "url": "/static/js/main.d0fa3f4e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
